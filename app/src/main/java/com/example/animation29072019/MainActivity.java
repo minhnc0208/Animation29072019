06497f1101bd5e416be00be5fbcbc965c9dfda5d
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-30 18:14:34 +0700

    'A

---
+++ app/src/main/java/com/example/animation29072019/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.animation29072019;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageviewAlpha);
+        final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_image_alpha);
+//        imageView.startAnimation(animation);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animation);
+
+            }
+        });
+    }
+}
