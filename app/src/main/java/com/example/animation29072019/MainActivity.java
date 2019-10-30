a9528d5e2648e614d4cd8cc2fb8e9e96829b9e8f
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-30 19:21:41 +0700

    'A'

--- app/src/main/java/com/example/animation29072019/MainActivity.java
+++ app/src/main/java/com/example/animation29072019/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView, imageView1;
+    ImageView imageView, imageView1, imageView2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView1 = findViewById(R.id.imageviewScale);
         imageView = findViewById(R.id.imageviewAlpha);
+        imageView2 = findViewById(R.id.imageviewRotate);
         //1 - alpha cho chuyen man hinh
 //        final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_image_alpha);
         // x < 50 => chạy về bên tay phải và ngược lại
         // y < 50 => chạy về bên dưới và ngược lại
-        final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_image_scale);
+        final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_image_rotate);
 //        imageView.startAnimation(animation);
 //        imageView.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -32,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 //        imageView1.startAnimation(animation);
-        imageView1.setOnClickListener(new View.OnClickListener() {
+//        imageView1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                view.startAnimation(animation);
+//
+//            }
+//        });
+        // y < 50: toa do nam tren va nguoc lai
+
+        // x < 50: toa do nam ben trai va nguoc lai
+        imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 view.startAnimation(animation);
